ca395cf45f6b48f2fc3d525fa47067d13b89481b
Ryan Hell <ryan_hell@outlook.com>
2021-03-05 19:26:44 -0500

    sws
    wsdw

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '@styles/globals.css'
+// pages/_app.js
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+ import '../styles/globals.css'
 
-export default Application
+ import "tailwindcss/tailwind.css";
+
+  function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
+
+  export default MyApp
