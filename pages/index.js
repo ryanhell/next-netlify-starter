9156b2deefeb7dc6808ecbb0b644ab649875b2ef
Ryan Hell <ryan_hell@outlook.com>
2021-03-05 19:10:56 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,346 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <IndexNavbar fixed />
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <div className="container mx-auto items-center flex flex-wrap">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+            <div className="pt-32 sm:pt-0">
+              <h2 className="font-semibold text-4xl text-gray-700">
+                Ryan Hell - <em>Tradesman, Analyst.</em>
+              </h2>
+              <p className="mt-4 text-lg leading-relaxed text-gray-600">
+                I have covered a lot of ground throughout my lifetime.
+                A technically inclined, communicative, and energetic contributer.
+              </p>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+              <div className="mt-12">
+                <a href="tel:555-555-5555">
+                  <button className="bg-green-500 text-white active:bg-green-600 font-bold uppercase text-base px-12 py-4 rounded shadow-lg hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                    <i className="fas fa-phone-square"></i>
+                     &nbsp; PHONE
+                </button>
+                </a>
+                <button className="bg-yellow-500 text-red active:bg-green-600 font-bold uppercase text-base px-12 py-4 rounded shadow-lg hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                  <i className="fas fa-envelope"></i> <a href="mailto:ryanbrooklyn2020@gmail.com">mail</a>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
+          src={require("assets/img/ryanhell1.png")}
+          alt="..."
+        />
+      </section>
 
+      <section className="mt-48 md:mt-40 pb-40 relative bg-gray-200">
+        <div
+          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
+          style={{ transform: "translateZ(0)" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-gray-200 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+        <div className="container mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-32">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-gray-800">
+                <img
+                  alt="..."
+                  src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+                  className="w-full align-middle rounded-t-lg"
+                />
+                <blockquote className="relative p-8 mb-4">
+                  <svg
+                    preserveAspectRatio="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 583 95"
+                    className="absolute left-0 w-full block h-95-px -top-94-px"
+                  >
+                    <polygon
+                      points="-30,95 583,95 583,65"
+                      className="text-gray-800 fill-current"
+                    ></polygon>
+                  </svg>
+                  <h4 className="text-xl font-bold text-white">
+                    .. .more experience, for less.
+                  </h4>
+                  <p className="text-md font-light mt-2 text-white">
+                    Putting together a team is expensive, and time consuming. Depending what part of the journey or lifecycle your prouct is in, having someone with a broader set of skills and experiences, is a better way to hire.
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+
+            <div className="w-full md:w-6/12 px-4">
+              <div className="flex flex-wrap">
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-sitemap"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Creative
+                      </h6>
+                      <p className="mb-4 text-gray-600">
+                        woodworker, carpenter &amp; software designer.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-drafting-compass"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Technical
+                      </h6>
+                      <p className="mb-4 text-gray-600">
+                        Systems engineering. Analysis. Mapping and knowledge transfer.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col min-w-0 mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-newspaper"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Visual</h6>
+                      <p className="mb-4 text-gray-600">
+                        visual analyst with keen eye for patterns, variations, as well as solving problems with predictable systematic approach
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-file-alt"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Organization
+                      </h6>
+                      <p className="mb-4 text-gray-600">
+                        SDLC, Six Sigma, LEAN, Agile and RAD experience. Both as a core contributor as well as scrum master, PM, lead, and architect.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container mx-auto px-4 pb-32 pt-48">
+          <div className="items-center flex flex-wrap">
+            <div className="w-full md:w-5/12 ml-auto px-12 md:px-4">
+              <div className="md:pr-12">
+                <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
+                  <i className="fas fa-file-alt text-xl"></i>
+                </div>
+                <h3 className="text-3xl font-semibold">
+                  Capable Research Analyst
+                </h3>
+                <p className="mt-4 text-lg leading-relaxed text-gray-600">
+                  Over two decades of providing research, presentations, &amp; persuasive materials for clients.
+                </p>
+                <ul className="list-none mt-6">
+                  <li className="py-2">
+                    <div className="flex items-center">
+                      <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-gray-100 mr-3">
+                          <i className="fas fa-fingerprint"></i>
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="text-gray-600">
+                          Primary Consultant Experience
+                        </h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="py-2">
+                    <div className="flex items-center">
+                      <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-gray-100 mr-3">
+                          <i className="fab fa-html5"></i>
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="text-gray-600">
+                          Dedicated to learning valuable new skills.
+                        </h4>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="py-2">
+                    <div className="flex items-center">
+                      <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-600 bg-gray-100 mr-3">
+                          <i className="far fa-paper-plane"></i>
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="text-gray-600">
+                          Lifelong Adult Learner
+                        </h4>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="w-full md:w-6/12 mr-auto px-4 pt-24 md:pt-0">
+              <img
+                alt="..."
+                className="max-w-full rounded-lg shadow-xl"
+                style={{
+                  transform:
+                    "scale(1) perspective(1040px) rotateY(-11deg) rotateX(2deg) rotate(2deg)",
+                }}
+                src={require("assets/img/ksa.jpg")}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="justify-center text-center flex flex-wrap mt-24">
+          <div className="w-full md:w-6/12 px-12 md:px-4">
+            <h2 className="font-semibold text-4xl">Recent Highlights</h2>
+            <p className="text-lg leading-relaxed mt-4 mb-4 text-gray-600">
+              Some of my favorite recent work here in Lancaster PA. Hoping to add more soon.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="block relative z-1 bg-gray-700">
+        <div className="container mx-auto">
+          <div className="justify-center flex flex-wrap">
+            <div className="w-full lg:w-12/12 px-4  -mt-24">
+              <div className="flex flex-wrap">
+                <div className="w-full lg:w-6/12 px-6">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    Custom Murphy Door
+                  </h5>
+
+                  <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                    <img
+                      alt="custom Murphy Door"
+                      className="align-middle border-none max-w-full h-auto rounded-lg"
+                      src={require("assets/img/portfolio/murphy1.png")}
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full lg:w-6/12 px-6">
+                  <h5 className="text-xl font-semibold pb-4 text-center">
+                    Portico Addition
+                  </h5>
+                  <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                    <img
+                      alt="..."
+                      className="align-middle border-none max-w-full h-auto rounded-lg"
+                      src={require("assets/img/portfolio/portico1.png")}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-gray-700 overflow-hidden">
+        <div className="container mx-auto pb-64">
+          <div className="flex flex-wrap justify-center">
+            <div className="w-full md:w-5/12 px-12 md:px-4 ml-auto mr-auto md:mt-64">
+              <div className="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
+                <i className="fas fa-code-branch text-xl"></i>
+              </div>
+              <h3 className="text-3xl mb-2 font-semibold leading-normal text-white">
+                OSHA Compliant
+              </h3>
+              <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-500">
+                I recieved 30 hour OSHA training in New York City in 2020.
+              </p>
+              <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-500">
+                I carry personal liability insurance.
+              </p>
+
+            </div>
+
+            <div className="w-full md:w-4/12 px-4 mr-auto ml-auto mt-32 relative">
+              <i className="fas fa-hard-hat text-gray-800 absolute text-55 -top-150-px -right-100 left-auto opacity-80"></i>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="pb-16 bg-gray-300 relative pt-32">
+        <div
+          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
+          style={{ transform: "translateZ(0)" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-gray-300 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+
+        <div className="container mx-auto">
+          <div className="flex flex-wrap justify-center bg-white shadow-xl rounded-lg -mt-64 py-16 px-12 relative z-10">
+            <div className="w-full text-center lg:w-8/12">
+              <h3 className="font-semibold text-3xl">
+                Not sure if I can help?
+              </h3>
+              <p className="text-gray-600 text-lg leading-relaxed mt-4 mb-4">
+                Lets connect and see if there is anything I can do to help. I'm interested in new industries &amp; open to new markets.
+              </p>
+              <div className="mt-12">
+                <a href="tel:555-555-5555">
+                  <button className="bg-green-500 text-white active:bg-green-600 font-bold uppercase text-base px-12 py-4 rounded shadow-lg hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                    <i class="fas fa-phone-square"></i>
+                     &nbsp; PHONE
+                </button>
+                </a>
+                <button className="bg-yellow-500 text-red active:bg-green-600 font-bold uppercase text-base px-12 py-4 rounded shadow-lg hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                  <i className="fas fa-envelope"></i> <a href="mailto:ryanbrooklyn2020@gmail.com">mail</a>
+                </button>
+              </div>
+              <div className="text-center mt-16"></div>
+            </div>
+          </div>
+        </div>
+      </section>
       <Footer />
-    </div>
-  )
+    </>
+  );
 }
